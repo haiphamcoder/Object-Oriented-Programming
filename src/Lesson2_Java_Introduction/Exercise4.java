88be3462ee35e106d944da219dde55729d454701
Hai Pham <ngochai285nd@gmail.com>
2022-10-16 22:34:26 +0700

    update

--- src/Lesson2_Java_Introduction/Exercise4.java
+++ src/Lesson2_Java_Introduction/Exercise4.java
@@ -3,9 +3,17 @@ package Lesson2_Java_Introduction;
 import java.util.Scanner;
 
 public class Exercise4 {
+    private static int[][] x;
+    private static int n;
+
     public static void main(String[] args) {
+        Input();
+        CompleteMatrix();
+        PrintMatrix();
+    }
+
+    private static void Input() {
         Scanner keyboard = new Scanner(System.in);
-        int n = 0;
         while (true) {
             try {
                 System.out.print("Nhap kich thuoc cua o vuong (3<=n<=8): ");
@@ -19,10 +27,11 @@ public class Exercise4 {
                 System.out.println("Khong hop le! Vui long nhap lai!");
             }
         }
+    }
 
-        int[][] x = new int[n + 1][n + 1];
-        int maxH = n, maxC = n, value = 1;
-        int minC = 1, minH = 1;
+    private static void CompleteMatrix() {
+        x = new int[n + 1][n + 1];
+        int maxH = n, maxC = n, minC = 1, minH = 1, value = 1;
         while (value <= n * n) {
             // Hang tren cung chua ghi
             for (int i = minC; i <= maxC; i++) {
@@ -48,7 +57,10 @@ public class Exercise4 {
             }
             minC++;
         }
+    }
 
+
+    private static void PrintMatrix() {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 System.out.printf("%-5d", x[i][j]);
