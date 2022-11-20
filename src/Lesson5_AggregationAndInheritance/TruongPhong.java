5eb8f549e182ab4905b547707b0cd2519ca0f23c
Hai Pham <ngochai285nd@gmail.com>
2022-11-21 06:07:17 +0700

    update

---
+++ src/Lesson5_AggregationAndInheritance/TruongPhong.java
@@ -0,0 +1,38 @@
+package Lesson5_AggregationAndInheritance;
+
+import Lesson3_Class.NhanVien;
+
+public class TruongPhong extends NhanVien {
+    // attributes
+    private double phuCap;
+    private int soNamDuongChuc;
+
+    // default constructor
+    public TruongPhong() {
+        super();
+    }
+
+    // parameterized constructor
+    public TruongPhong(String tenNhanVien, double luongCoBan, double heSoLuong, double phuCap, int soNamDuongChuc) {
+        super(tenNhanVien, luongCoBan, heSoLuong, 5000);
+        this.phuCap = phuCap;
+        this.soNamDuongChuc = soNamDuongChuc;
+    }
+
+    // accesor and mutator methods
+    public double getPhuCap() {
+        return phuCap;
+    }
+
+    public void setPhuCap(double phuCap) {
+        this.phuCap = phuCap;
+    }
+
+    public int getSoNamDuongChuc() {
+        return soNamDuongChuc;
+    }
+
+    public void setSoNamDuongChuc(int soNamDuongChuc) {
+        this.soNamDuongChuc = soNamDuongChuc;
+    }
+}
